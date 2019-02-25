095eec5b904454b29d83ece57856bbe153c1da22
qinggeouye <qinggeouye@gmail.com>
2019-02-25 23:05:14 +0800

    14 双向广度优先搜索 完善两个方法

--- MathematicProgrammer/14_breadthFirstSearch/lesson14_1.py
+++ MathematicProgrammer/14_breadthFirstSearch/lesson14_1.py
@@ -1,4 +1,5 @@
 import queue
+import random
 
 
 class Node(object):
@@ -6,22 +7,66 @@ class Node(object):
         self.user_id = user_id
         self.friends = set()
         self.degree = 0
-        self.degrees = dict({user_id: 0})  # 从不同用户出发，到当前用户是第几度
 
 
-def get_next_degree_friend(user_id, user_nodes, que, visited, degree):
+def set_user_relation(user_num, relation_num):
+    """
+    随机生成用户间的关系
+    :param user_num: 用户数量 即节点的数量
+    :param relation_num: 好友关系的数量 即边的数量
+    :return:
+    """
+    # 生成所有表示用户的节点
+    use_nodes = list([None] * user_num)
+    for u in range(user_num):
+        use_nodes[u] = Node(u)
 
-    return que, visited
+    # 生成所有表示好友关系的边
+    for r in range(relation_num):
+        friend_a_id = random.randrange(user_num)
+        friend_b_id = random.randrange(user_num)
+        # 自己不能是自己的好友 如果生成的两个好友 id 相同，跳过
+        if friend_a_id == friend_b_id:
+            continue
+        friend_a = use_nodes[friend_a_id]
+        friend_b = use_nodes[friend_b_id]
+
+        friend_a.friends.add(friend_b_id)
+        friend_b.friends.add(friend_a_id)
+    return use_nodes
+
+
+def get_next_degree_friend(user_nodes, que, visited):
+    """
+    :param user_nodes: 用户节点网络
+    :param que: 某一层用户节点 即第几度好友
+    :param visited: 已访问的所有用户节点
+    :return:
+    """
+    que_return = queue.Queue()  # 只保存某个用户的第几度好友
+    visited_return = set()  # 保存从某个用户开始到第几度好友
+    while not que.empty():
+        current_user_id = que.get()
+        if user_nodes[current_user_id] is None:
+            continue
+        for friend_id in user_nodes[current_user_id].friends:
+            if user_nodes[friend_id] is None:
+                continue
+            if friend_id in visited:
+                continue
+            que_return.put(friend_id)
+            visited_return.add(friend_id)  # 记录已经访问过的节点
+    return que_return, visited_return
 
 
 def has_overlap(visited_a, visited_b):
-
-    return False
+    # 两个 set() 的交集
+    return len(visited_a & visited_b) > 0
 
 
 def bi_bfs(user_nodes, user_id_a, user_id_b):
     """
-    双向广度优先搜索 查找两人之间最短通路到长度
+    双向广度优先搜索 查找两人之间最短通路的长度
     :param user_nodes: 所有用户节点
     :param user_id_a: 用户 a
     :param user_id_b: 用户 b
@@ -37,11 +82,11 @@ def bi_bfs(user_nodes, user_id_a, user_id_b):
     que_a = queue.Queue()  # 队列 a，用于从用户 a 出发的广度优先搜索
     que_b = queue.Queue()  # 队列 b，用于从用户 b 出发的广度优先搜索
 
-    que_a.put(user_id_a)  # 放入初始节点
+    que_a.put(user_id_a)  # 放入初始节点 a
     visited_a = set()  # 存放已经被访问的节点，防止回路
     visited_a.add(user_id_a)
 
-    que_b.put(user_id_b)
+    que_b.put(user_id_b)  # 放入初始节点 b
     visited_b = set()
     visited_b.add(user_id_b)
 
@@ -50,13 +95,22 @@ def bi_bfs(user_nodes, user_id_a, user_id_b):
     while degree_a + degree_b < max_degree:
         degree_a += 1
         # 沿着 a 出发的方向，继续广度优先搜索 degree+1 的好友
-        que_a, visited_a = get_next_degree_friend(user_id_a, user_nodes, que_a, visited_a, degree_a)
-        # 判断目前为止，被发现的 a 的好友，和被发现的 b 的好友，两个合集是否存在交集
+        que_a, visited_a = get_next_degree_friend(user_nodes, que_a, visited_a)
+        # 判断到目前为止，被发现的 a 的好友，和被发现的 b 的好友，两个合集是否存在交集
         if has_overlap(visited_a, visited_b):
             return degree_a + degree_b
+
         degree_b += 1
-        que_b, visited_b = get_next_degree_friend(user_id_b, user_nodes, que_b, visited_b, degree_b)
+        que_b, visited_b = get_next_degree_friend(user_nodes, que_b, visited_b)
         if has_overlap(visited_a, visited_b):
             return degree_a + degree_b
-    # 广度优先搜素超过 max_degree 之后，仍然没有发现 a 和 b 的重叠，认为没有通路
+    # 广度优先搜索超过 max_degree 之后，仍然没有发现 a 和 b 的重叠，认为没有通路
     return -1
+
+
+if __name__ == "__main__":
+    user_nodes_list = set_user_relation(10, 20)
+    for i in range(len(user_nodes_list)):
+        print("用户 %s 的好友: %s" % (user_nodes_list[i].user_id, user_nodes_list[i].friends))
+    print("---------双向广度优先搜索---------")
+    print("两个用户节点之间的最短路径长度：", bi_bfs(user_nodes_list, 1, 2))
