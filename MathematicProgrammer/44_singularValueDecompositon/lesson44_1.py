952247d817ad077060d2789beaf52811eec89506
qinggeouye <qinggeouye@gmail.com>
2019-04-04 23:50:26 +0800

    44 奇异值分解SVD 潜在语义分析LSA (3)

--- MathematicProgrammer/44_singularValueDecompositon/lesson44_1.py
+++ MathematicProgrammer/44_singularValueDecompositon/lesson44_1.py
@@ -8,7 +8,7 @@ from numpy import linalg as la
 
 # 文档集合 文档和词条关系矩阵 行表示文档 列表示词条
 x = np.mat([[1, 1, 1, 0, 0], [2, 2, 2, 0, 0],
-            [1, 1, 1, 0, 0], [5, 5, 5, 0, 5],
+            [1, 1, 1, 0, 0], [5, 5, 5, 0, 0],
             [0, 0, 0, 2, 2], [0, 0, 0, 3, 3],
             [0, 0, 0, 1, 1]])
 
@@ -20,16 +20,16 @@ u_eig_values, u_eig_vectors = np.linalg.eig(x.dot(x.transpose()))
 u_eig_vectors = u_eig_vectors[:, np.argsort(u_eig_values)[::-1]]  # 降序排序
 u_eig_values = np.sort(u_eig_values)[::-1]  # 降序排序
 # u_eig_values = u_eig_values[u_eig_values > 1e-5]
-print(" 特征值：", u_eig_values)
-print(" 左奇异向量: \n", u_eig_vectors, "\n")
+print(" 特征值：", np.around(u_eig_values, decimals=4))
+print(" 左奇异向量: \n", np.around(u_eig_vectors, decimals=2), "\n")
 
 # 右奇异向量 X'X -> V
 v_eig_values, v_eig_vectors = np.linalg.eig(x.transpose().dot(x))
 v_eig_vectors = v_eig_vectors[:, np.argsort(v_eig_values)[::-1]]  # 降序排序
 v_eig_values = np.sort(v_eig_values)[::-1]  # 降序排序
 # v_eig_values = v_eig_values[v_eig_values > 1e-5]
-print(" 特征值：", v_eig_values)
-print(" 右奇异向量: \n", v_eig_vectors, "\n")
+print(" 特征值：", np.around(v_eig_values, decimals=4))
+print(" 右奇异向量: \n", np.around(v_eig_vectors, decimals=2), "\n")
 
 
 # 奇异值矩阵 S , 对角线非零元素 奇异值 σ
@@ -38,9 +38,9 @@ print(" 右奇异向量: \n", v_eig_vectors, "\n")
 # 特征值的求法-> |XX' - λI| = |(XX'-λI)'| = |X'X - λI| = 0
 # 因为 XX' = (USV')(VS'U') = U(SS')U' , 则奇异值 σ 是 XX' 非零特征值的平方根
 s_eig_values = np.sqrt(u_eig_values[u_eig_values > 0])
-print(" 奇异值: ", s_eig_values)
+print(" 奇异值: ", np.around(s_eig_values, decimals=4))
 s_eig_values2 = np.sqrt(v_eig_values[v_eig_values > 0])
-print(" 奇异值: ", s_eig_values2, "\n")
+print(" 奇异值: ", np.around(s_eig_values2, decimals=4), "\n")
 S = np.zeros((7, 5))  # 奇异矩阵
 for i in range(len(s_eig_values)):
     S[i, i] = s_eig_values[i]
@@ -54,11 +54,11 @@ print("\n--------------- numpy 库的 linalg 已经实现 SVD ---------------\n"
 U, sigma, VT = la.svd(x)
 print(U, "\n")
 print(sigma, "\n")
-print(VT, "\n")
+print(VT, "\n")  # 注意 VT 已经转置
 S2 = np.zeros((7, 5))  # 奇异矩阵
 for i in range(len(sigma)):
     S2[i, i] = sigma[i]
 print(" 酉矩阵: \n", U.transpose().dot(U))  # 酉矩阵
 print(" 酉矩阵: \n", VT.transpose().dot(VT))  # 酉矩阵
-print(" 矩阵还原: \n", U.dot(S2).dot(VT.transpose()))
+print(" 矩阵还原: \n", U.dot(S2).dot(VT))
 
