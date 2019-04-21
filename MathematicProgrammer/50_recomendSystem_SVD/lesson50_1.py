6918aa4ecd3bda9e0c403f85317af3fd62f9088b
qinggeouye <qinggeouye@gmail.com>
2019-04-22 03:48:19 +0800

    50 推荐系统-SVD奇异值分解（MovieLens） running time

--- MathematicProgrammer/50_recomendSystem_SVD/lesson50_1.py
+++ MathematicProgrammer/50_recomendSystem_SVD/lesson50_1.py
@@ -1,13 +1,17 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import scale
+import time
+
+# 运行开始时间
+time_start = time.time()
 
 # 加载用户对电影的评分数据
 df_ratings = pd.read_csv("ml-latest-small/ratings.csv")
 
 # 获取用户的数量和电影的数量，这里我们只取前 1/10 来减小数据规模
-user_num = int(df_ratings["userId"].max() / 10)
-movie_num = int(df_ratings["movieId"].max() / 10)
+user_num = int(df_ratings["userId"].max() / 2)
+movie_num = int(df_ratings["movieId"].max() / 2)
 print(user_num, movie_num)
 
 # 构造用户对电影的二元关系矩阵
@@ -32,20 +36,24 @@ x = np.mat(user_rating)
 x_s = scale(x, with_mean=True, with_std=True, axis=1)
 
 u, sigma, vt = np.linalg.svd(x_s, full_matrices=False, compute_uv=True)
-print("U 矩阵：", u)
+# print("U 矩阵：", u)
 print("Sigma 奇异值：", sigma)
-print("V 矩阵：", vt)
+# print("V 矩阵：", vt)
 
 # 加载电影元信息
 df_movies = pd.read_csv("ml-latest-small/movies.csv")
 dict_movies = dict()
 for index, row in df_movies.iterrows():
     dict_movies[row["movieId"]] = "{0}, {1}".format(row["title"], row["genres"])
-print(dict_movies)
+# print(dict_movies)
 
 # 输出和某个奇异值高度相关的电影，这些电影代表了一个主题
 # (注意：向量中电影的 ID 和原始的电影的 ID 相差 1，所以在读取 dict_movies 需要使用 i+1)
 print(np.max(vt[1,:]))
 for i in range(movie_num):
-    if (vt[1][i] > 0.1):
+    if vt[1][i] > 0.1:
         print(i + 1, vt[1][i], dict_movies[i + 1])
+
+# 运行结束时间
+time_end = time.time()
+print("程序运行耗时：", time_end - time_start)
--- MathematicProgrammer/50_recomendSystem_SVD/lesson50_2.py
+++ MathematicProgrammer/50_recomendSystem_SVD/lesson50_2.py
@@ -1,14 +1,17 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import scale
+import time
 
+# 运行开始时间
+time_start = time.time()
 
 # 加载用户对电影对评分数据
 df_ratings = pd.read_csv("ml-latest-small/ratings.csv")
 
 # 获取用户对数量和电影对数量 这里只取前 1/10 , 减少数据规模
-user_num = int(df_ratings["userId"].max() / 10)
-movie_num = int(df_ratings["movieId"].max() / 10)
+user_num = int(df_ratings["userId"].max() / 2)
+movie_num = int(df_ratings["movieId"].max() / 2)
 print(user_num, movie_num)
 df_ratings = df_ratings[df_ratings["userId"] <= user_num]
 df_ratings = df_ratings[df_ratings["movieId"] <= movie_num]
@@ -32,17 +35,21 @@ x_s = scale(x, with_mean=True, with_std=True, axis=1)
 
 # 进行 SVD 奇异值分解
 u, sigma, vt = np.linalg.svd(x_s, full_matrices=False, compute_uv=True)
-print("U 矩阵：", u)
+# print("U 矩阵：", u)
 print("Sigma 奇异值：", sigma)
-print("V 矩阵：", vt)
+# print("V 矩阵：", vt)
 
 # 加载电影元信息
 df_movies = pd.read_csv("ml-latest-small/movies.csv")
 dict_movies = dict(zip(df_movies["movieId"], df_movies["title"] + ", " + df_movies["genres"]))
-print(dict_movies)
+# print(dict_movies)
 
 # 输出和某个奇异值高度相关的电影 这些电影代表了一个主题
 # (注意：向量中电影的 ID 和原始的电影的 ID 相差 1，所以在读取 dict_movies 需要使用 i+1)
 print(np.max(vt[1, :]))
 print(list(zip(np.where(vt[1] > 0.1)[0] + 1, vt[1][np.where(vt[1] > 0.1)],
                [dict_movies[i] for i in (np.where(vt[1] > 0.1)[0] + 1)])))
+
+# 运行结束时间
+time_end = time.time()
+print("程序运行耗时：", time_end - time_start)
